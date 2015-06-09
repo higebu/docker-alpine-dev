fb13aa5a8c980d677c5b7b33234af83584027a9f
Yuya Kusakabe <yuya.kusakabe@gmail.com>
2015-06-09 14:13:59 +0900

    Fix permission of docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,9 +3,11 @@
 git config --global user.name "$USER_NAME"
 git config --global user.email "$USER_EMAIL"
 
-sed -i "s/^#PACKAGER/PACKAGER=\"$USER_NAME <$USER_EMAIL>\"/" /etc/abuild.conf
+sed -i "s/^#PACKAGER=\"Your Name <your@email.address>\"/PACKAGER=\"$USER_NAME <$USER_EMAIL>\"/" /etc/abuild.conf
 
 git clone git://dev.alpinelinux.org/aports
 
 mkdir -p /var/cache/distfiles
 chmod a+w /var/cache/distfiles
+
+tail -f /dev/null
