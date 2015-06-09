c5a7a4e40a6fa5b73d9ccca99188116034976429
Yuya Kusakabe <yuya.kusakabe@gmail.com>
2015-06-09 15:55:43 +0900

    Add sudo to docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,11 +5,11 @@ git config --global user.email "$USER_EMAIL"
 git config --global color.ui true
 git config --global core.pager vim
 
-sed -i "s/^#PACKAGER=\"Your Name <your@email.address>\"/PACKAGER=\"$USER_NAME <$USER_EMAIL>\"/" /etc/abuild.conf
+sudo sed -i "s/^#PACKAGER=\"Your Name <your@email.address>\"/PACKAGER=\"$USER_NAME <$USER_EMAIL>\"/" /etc/abuild.conf
 
 git clone git://dev.alpinelinux.org/aports
 
-mkdir -p /var/cache/distfiles
-chmod a+w /var/cache/distfiles
+sudo mkdir -p /var/cache/distfiles
+sudo chmod a+w /var/cache/distfiles
 
 tail -f /dev/null
